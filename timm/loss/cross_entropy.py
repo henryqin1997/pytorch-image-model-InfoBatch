cde1116f811b65cbde3bed96e0fb5c4d40aa558c
henryqin1997 <henryqin1997@gamil.com>
2023-04-05 15:57:56 +0800

    fix import

--- timm/loss/cross_entropy.py
+++ timm/loss/cross_entropy.py
@@ -42,7 +42,7 @@ class SoftTargetCrossEntropy(nn.Module):
 class SoftTargetCrossEntropyNoReduction(nn.Module):
 
     def __init__(self):
-        super(SoftTargetCrossEntropy, self).__init__()
+        super(SoftTargetCrossEntropyNoReduction, self).__init__()
 
     def forward(self, x: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         loss = torch.sum(-target * F.log_softmax(x, dim=-1), dim=-1)
