8e3a629e9e49b587eb7980abec10e7771641b6d4
henryqin1997 <henryqin1997@gamil.com>
2023-04-27 16:51:31 +0800

    infobatch v2 for test, update score estimation

--- distributed_train_infobatch_strongaug.sh
+++ distributed_train_infobatch_strongaug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUM_PROC=$1
 shift
-torchrun --rdzv-endpoint=localhost:13344 --nproc_per_node=$NUM_PROC train_infobatch_strongaug.py "$@"
+torchrun --master_port 13344 --nproc_per_node=$NUM_PROC train_infobatch_strongaug.py "$@"
